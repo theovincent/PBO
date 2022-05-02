b5f7b7408a2d0d4da277e46427e6a06521aa01e6
Théo <theo.vincent@eleves.enpc.fr>
2022-05-02 14:16:40 +0200

    Start investigation

---
+++ pbo/state_action_mesh/tries.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class StateActionMesh:
+    def __init__(self, action_max: float, n_actions: int, state_max: float, n_states: int) -> "StateActionMesh":
+        self.action_max = action_max
+        self.n_actions = n_actions
+        self.state_max = state_max
+        self.n_states = n_states
+
+        self.fig, self.ax = plt.subplots()
+        self.values = np.zeros((self.n_states, self.n_actions))
+
+    def set_values(self, values: np.ndarray) -> None:
+        assert values.shape == (
+            self.n_states,
+            self.n_actions,
+        ), f"given shape values: {values.shape} don't match with environment values: {(self.n_states, self.n_actions)}"
+
+        self.values = values
+
+    def set_title(self, title: str) -> None:
+        self.set_title(title)
+
+    def show(self):
+        colors = self.ax.pcolor(self.values)
+        self.fig.colorbar(colors, ax=self.ax)
+        self.fig.tight_layout()
