18edf6f7089318160e5f0bc817b8600312d699b3
Théo <theo.vincent@eleves.enpc.fr>
2022-05-10 11:20:03 +0200

    LQR understood

--- pbo/environment/linear_quadratic.py
+++ pbo/environment/linear_quadratic.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg as sc_linalg
 
 from mushroom_rl.core import Environment, MDPInfo
 from mushroom_rl.utils import spaces
@@ -12,14 +13,14 @@ class LinearQuadraticEnv(Environment):
     The system equations in this task are:
 
     .. math::
-        x_{t+1} = Ax_t + Bu_t
+        s_{t+1} = As_t + Ba_t
 
-    where x is the state and u is the control signal.
+    where s is the state and a is the control signal.
 
     The reward function is given by:
 
     .. math::
-        r_t = \\left( x_t^TQx_t + u_t^TRu_t + 2 x_t^TSu_t \\right)
+        r_t = \\left( s_t^TQs_t + a_t^TRa_t + 2 s_t^TSa_t \\right)
 
     "Policy gradient approaches for multi-objective sequential decision making".
     Parisi S., Pirotta M., Smacchia N., Bascetta L., Restelli M.. 2014
@@ -28,11 +29,6 @@ class LinearQuadraticEnv(Environment):
 
     def __init__(
         self,
-        A,
-        B,
-        Q,
-        R,
-        S,
         max_pos=np.inf,
         max_action=np.inf,
         random_init=False,
@@ -59,11 +55,31 @@ class LinearQuadraticEnv(Environment):
                 horizon (int, 50): horizon of the mdp.
 
         """
-        self.A = A
-        self.B = B
-        self.Q = Q
-        self.R = R
-        self.S = S
+        # Generate a controllable environmnent
+        controllable = False
+
+        while not controllable:
+            self.A = np.random.uniform(-10, 10, size=(1, 1))
+            self.B = np.random.uniform(-10, 10, size=(1, 1))
+            self.Q = np.random.uniform(-10, 0, size=(1, 1))
+            self.R = np.random.uniform(-10, 10, size=(1, 1))
+            self.S = np.random.uniform(-5, 5, size=(1, 1))
+
+            self.P = sc_linalg.solve_discrete_are(self.A, self.B, self.Q, self.R, s=self.S)
+            self.R_hat = self.R + self.B.T @ self.P @ self.B
+
+            if self.R_hat < 0:
+                controllable = True
+                self.S_hat = self.S + self.A.T @ self.P @ self.B
+                self.K = sc_linalg.inv(self.R_hat) @ self.S_hat.T
+
+                print("Transition: s' = As + Ba")
+                print(f"Transition: s' = {np.around(self.A[0, 0], 2)}s + {np.around(self.B[0, 0], 2)}a")
+                print("Reward: Qs² + Ra² + 2 Ssa")
+                print(
+                    f"Reward: {np.around(self.Q[0, 0], 2)}s² + {np.around(self.R[0, 0], 2)}a² + {np.around(2 * self.S[0, 0], 2)}sa"
+                )
+
         self._max_pos = max_pos
         self._max_action = max_action
         self._episodic = episodic
@@ -72,10 +88,10 @@ class LinearQuadraticEnv(Environment):
         self._initial_state = initial_state
 
         # MDP properties
-        high_x = self._max_pos * np.ones(A.shape[0])
+        high_x = self._max_pos * np.ones(self.A.shape[0])
         low_x = -high_x
 
-        high_u = self._max_action * np.ones(B.shape[1])
+        high_u = self._max_action * np.ones(self.B.shape[1])
         low_u = -high_u
 
         observation_space = spaces.Box(low=low_x, high=high_x)
@@ -103,17 +119,17 @@ class LinearQuadraticEnv(Environment):
         return self._state
 
     def step(self, action):
-        x = self._state
-        u = self._bound(action, self.info.action_space.low, self.info.action_space.high)
+        s = self._state
+        a = self._bound(action, self.info.action_space.low, self.info.action_space.high)
 
-        reward = x.dot(self.Q).dot(x) + u.dot(self.R).dot(u) + 2 * x.dot(self.S).dot(u)
-        self._state = self.A.dot(x) + self.B.dot(u)
+        reward = s.T @ self.Q @ s + a.T @ self.R @ a + 2 * s.T @ self.S @ a
+        self._state = self.A @ s + self.B @ a
 
         absorbing = False
 
         if np.any(np.abs(self._state) > self._max_pos):
             if self._episodic:
-                reward = self._max_pos**2 * 10
+                reward = -self._max_pos**2 * 10
                 absorbing = True
             else:
                 self._state = self._bound(
@@ -121,3 +137,6 @@ class LinearQuadraticEnv(Environment):
                 )
 
         return self._state, reward, absorbing, {}
+
+    def optimal_action(self):
+        return -self.K @ self._state
