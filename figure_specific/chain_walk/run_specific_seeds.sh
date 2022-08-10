68628b3516c75b53ec3360b8e1214b2f620a33ad
vincent <theo.vincent@eleves.enpc.fr>
2022-08-10 17:36:16 +0200

    Chain Walk end

--- figure_specific/chain_walk/run_specific_seeds.sh
+++ figure_specific/chain_walk/run_specific_seeds.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# PBO linear
+# PBO max linear
 counter=1
 while [ $counter -le  $1 ]
 do
-    jupyter nbconvert --to notebook --inplace --execute figure_specific/chain_walk/PBO_linear.ipynb
-    echo PBO linear: $counter out of $1 runs
+    jupyter nbconvert --to notebook --inplace --execute figure_specific/chain_walk/PBO_max_linear.ipynb
+    echo PBO max linear: $counter out of $1 runs
     ((counter++))
 done
