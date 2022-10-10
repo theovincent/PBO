5d08a0df02f12e36ab0d05fbeb5896b545b1be67
vincent <theo.vincent@eleves.enpc.fr>
2022-10-10 18:05:46 +0200

    GPU working

--- figure_specific/bicycle/run_seeds.sh
+++ figure_specific/bicycle/run_seeds.sh
@@ -6,23 +6,23 @@ parse_arguments $@
 edit_json -f figure_specific/bicycle/parameters.json -k max_bellman_iterations -v $N_BI
 
 
-# FQI
-counter=1
-while [ $counter -le  $N_SEEDS ]
-do
-    jupyter nbconvert --to notebook --inplace --execute figure_specific/bicycle/FQI.ipynb
-    echo FQI: $counter out of $N_SEEDS runs
-    ((counter++))
-done
+# # FQI
+# counter=1
+# while [ $counter -le  $N_SEEDS ]
+# do
+#     jupyter nbconvert --to notebook --inplace --execute figure_specific/bicycle/FQI.ipynb
+#     echo FQI: $counter out of $N_SEEDS runs
+#     ((counter++))
+# done
 
-# PBO linear
-counter=1
-while [ $counter -le  $N_SEEDS ]
-do
-    jupyter nbconvert --to notebook --inplace --execute figure_specific/bicycle/PBO_linear.ipynb
-    echo PBO linear: $counter out of $N_SEEDS runs
-    ((counter++))
-done
+# # PBO linear
+# counter=1
+# while [ $counter -le  $N_SEEDS ]
+# do
+#     jupyter nbconvert --to notebook --inplace --execute figure_specific/bicycle/PBO_linear.ipynb
+#     echo PBO linear: $counter out of $N_SEEDS runs
+#     ((counter++))
+# done
 
 # PBO linear max linear
 counter=1
