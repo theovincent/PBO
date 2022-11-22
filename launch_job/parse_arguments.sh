eabfca6171c8c475e41d346b5589a86ddc2e03c5
vincent <theo.vincent@eleves.enpc.fr>
2022-11-22 14:10:54 +0100

    seed launcher ready

--- launch_job/parse_arguments.sh
+++ launch_job/parse_arguments.sh
@@ -3,6 +3,16 @@
 function parse_arguments() {
     while [[ $# -gt 0 ]]; do
         case $1 in
+            -fs | --first_seed)
+                FIRST_SEED=$2
+                shift
+                shift
+                ;;
+            -ls | --last_seed)
+                LAST_SEED=$2
+                shift
+                shift
+                ;;
             -s | --seed)
                 SEED=$2
                 shift
@@ -22,12 +32,24 @@ function parse_arguments() {
                 shift
                 shift
                 ;;
+            ?*)
+                printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+                shift
+                ;;
         esac
     done
 
-    if [[ $SEED == "" ]]
+    if [[ $SEED == "" ]] && [[ $FIRST_SEED == "" ]] && [[ $LAST_SEED == "" ]]
     then
-        echo "seed is missing, use -s" >&2
+        echo "seed information is missing, use -s or (-fs and -ls)" >&2
+        exit
+    elif [[ $SEED != "" ]] && ( [[ $FIRST_SEED != "" ]] || [[ $LAST_SEED != "" ]] )
+    then
+        echo "you need to choose between -s and (-fs and -ls)" >&2
+        exit
+    elif [[ $SEED == "" ]] && ( [[ $FIRST_SEED == "" ]] || [[ $LAST_SEED == "" ]] )
+    then
+        echo "you need to specify -fs and -ls" >&2
         exit
     elif [[ $MAX_BELLMAN_ITERATION == "" ]]
     then
