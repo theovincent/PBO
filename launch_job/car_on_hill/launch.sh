ee612e14836e30d6910c0ebe067ad44a1ac8d338
vincent <theo.vincent@eleves.enpc.fr>
2022-11-22 12:01:55 +0100

    train without gpu

--- launch_job/car_on_hill/launch.sh
+++ launch_job/car_on_hill/launch.sh
@@ -9,13 +9,13 @@ then
 fi
 
 
-submission_train_fqi=$(sbatch -J train_fqi --mem-per-cpu=8Gc --time=04:30 --gpus=1 --output=out/train_fqi_$seed.out launch_job/car_on_hill/train_fqi.sh -s $SEED -b $MAX_BELLMAN_ITERATION)
+submission_train_fqi=$(sbatch -J train_fqi --mem-per-cpu=8Gc --time=04:30 --output=out/train_fqi_$seed.out launch_job/car_on_hill/train_fqi.sh -s $SEED -b $MAX_BELLMAN_ITERATION)
 
-submission_train_pbo_linear=$(sbatch -J train_pbo_linear --mem-per-cpu=8Gc --time=02:30 --gpus=1 --output=out/train_pbo_linear_$seed.out launch_job/car_on_hill/train_pbo_linear.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a linear)
+submission_train_pbo_linear=$(sbatch -J train_pbo_linear --mem-per-cpu=8Gc --time=02:30 --output=out/train_pbo_linear_$seed.out launch_job/car_on_hill/train_pbo_linear.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a linear)
 submission_evaluate_pbo_linear=$(sbatch -J evaluate_pbo_linear --dependency=afterok:$submission_train_pbo_linear --mem-per-cpu=4Gc --time=02:30 --output=out/evaluate_pbo_linear_$seed.out launch_job/car_on_hill/evaluate_pbo_linear.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a linear)
 
-submission_train_pbo_deep=$(sbatch -J train_pbo_deep --mem-per-cpu=8Gc --time=02:30 --gpus=1 --output=out/train_pbo_deep_$seed.out launch_job/car_on_hill/train_pbo_deep.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a deep)
+submission_train_pbo_deep=$(sbatch -J train_pbo_deep --mem-per-cpu=8Gc --time=02:30 --output=out/train_pbo_deep_$seed.out launch_job/car_on_hill/train_pbo_deep.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a deep)
 submission_evaluate_pbo_deep=$(sbatch -J evaluate_pbo_deep --dependency=afterok:$submission_train_pbo_deep --mem-per-cpu=4Gc --time=02:30 --output=out/evaluate_pbo_deep_$seed.out launch_job/car_on_hill/evaluate_pbo_deep.sh -s $SEED -b $MAX_BELLMAN_ITERATION -a deep)
 
-submission_train_ifqi=$(sbatch -J train_ifqi --mem-per-cpu=8Gc --time=02:30 --gpus=1 --output=out/train_ifqi_$seed.out launch_job/car_on_hill/train_ifqi.sh -s $SEED -b $MAX_BELLMAN_ITERATION)
+submission_train_ifqi=$(sbatch -J train_ifqi --mem-per-cpu=8Gc --time=02:30 --output=out/train_ifqi_$seed.out launch_job/car_on_hill/train_ifqi.sh -s $SEED -b $MAX_BELLMAN_ITERATION)
 submission_evaluate_ifqi=$(sbatch -J evaluate_ifqi --dependency=afterok:$submission_train_ifqi --mem-per-cpu=4Gc --time=02:30 --output=out/evaluate_ifqi_$seed.out launch_job/car_on_hill/evaluate_ifqi.sh -s $SEED -b $MAX_BELLMAN_ITERATION)
