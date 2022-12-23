fa97157addd3689f89b06ac97b5060076eb38a91
Theo <theovincentjourdat@gmail.com>
2022-12-23 14:24:43 +0100

    Launch IAS cluster done

--- launch_job/lunar_lander/train_dqn.sh
+++ launch_job/lunar_lander/train_dqn.sh
@@ -5,4 +5,4 @@ parse_arguments $@
 
 source env_cpu/bin/activate
 
-lunar_lander_fqi -e $EXPERIMENT_NAME -s $SLURM_ARRAY_TASK_ID -b $MAX_BELLMAN_ITERATION
+lunar_lander_dqn -e $EXPERIMENT_NAME -s $SLURM_ARRAY_TASK_ID -b $MAX_BELLMAN_ITERATION
