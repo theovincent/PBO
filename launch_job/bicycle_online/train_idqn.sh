f7e76f8665563770fc71f0fa3919af8480e21199
Theo <theovincentjourdat@gmail.com>
2023-01-13 15:45:52 +0100

    Launcher for bicycle online

---
+++ launch_job/bicycle_online/train_idqn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source launch_job/parse_arguments.sh
+parse_arguments $@
+
+if [[ $GPU = true ]]
+then
+    source env_gpu/bin/activate
+else
+    source env_cpu/bin/activate
+fi 
+
+bicycle_online_idqn -e $EXPERIMENT_NAME -s $SLURM_ARRAY_TASK_ID -b $MAX_BELLMAN_ITERATION
