97a4d9fa83ab9a7aab0ad76126a31f2c121545a7
Theo <theovincentjourdat@gmail.com>
2023-01-17 12:03:27 +0100

    Clean plots

---
+++ experiments/__init__.py
@@ -0,0 +1,13 @@
+colors = {
+    "LSPI": "#984ea3",
+    "FQI": "#e41a1c",
+    "DQN": "#e41a1c",
+    "ProFQI": "#4daf4a",
+    "ProDQN": "#4daf4a",
+    "blue": "#377eb8",
+    "orange": "#ff7f00",
+    "pink": "#f781bf",
+    "brown": "#a65628",
+    "grey": "#999999",
+    "yellow": "#dede00",
+}
