951c1dd2ae421808427f2997e170bde4cd219026
Theo <theovincentjourdat@gmail.com>
2023-01-18 16:01:58 +0100

    Acrobot

---
+++ launch_job/acrobot/evaluate_pbo_deep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source launch_job/parse_arguments.sh
+parse_arguments $@
+
+source env_cpu/bin/activate 
+
+acrobot_pbo_evaluate -e $EXPERIMENT_NAME -s $SLURM_ARRAY_TASK_ID -b $MAX_BELLMAN_ITERATION -a deep $CONV
