4ecc3a63b4cf9ef968982bc47fcaf391276d2f30
Theo Vincent <theovincentjourdat@gmail.com>
2023-08-13 22:19:21 +0200

    update launcher

--- launch_job/lunar_lander/grid_search_prodqn.sh
+++ launch_job/lunar_lander/grid_search_prodqn.sh
@@ -3,14 +3,18 @@
 
 for TARGET_UPDATES in 30 100
 do
-    for WEIGHTS_UPDATES in 4000 6000
-    do 
-        EXPERIMENT_NAME=pbo2_2_2_2_lr5_t$TARGET_UPDATES\_w15_u$WEIGHTS_UPDATES
+    for N_WEIGHTS in 1 20
+    do  
+        for WEIGHTS_UPDATES in 3000 5000
+        do 
+            EXPERIMENT_NAME=pbo2_2_2_2_lr5_t$TARGET_UPDATES\_w$N_WEIGHTS\_u$WEIGHTS_UPDATES
 
-        PARAMS=$(jq '.prodqn_n_training_steps_per_target_update = $TARGET_UPDATES' --argjson TARGET_UPDATES $TARGET_UPDATES experiments/lunar_lander/parameters.json)
-        PARAMS=$(jq '.prodqn_n_training_steps_per_current_weight_update = $WEIGHTS_UPDATES' --argjson WEIGHTS_UPDATES $WEIGHTS_UPDATES <<<"$PARAMS")
-        echo $PARAMS > experiments/lunar_lander/parameters.json
+            PARAMS=$(jq '.prodqn_n_training_steps_per_target_update = $TARGET_UPDATES' --argjson TARGET_UPDATES $TARGET_UPDATES experiments/lunar_lander/parameters.json)
+            PARAMS=$(jq '.prodqn_n_current_weights = $N_WEIGHTS' --argjson N_WEIGHTS $N_WEIGHTS <<<"$PARAMS")
+            PARAMS=$(jq '.prodqn_n_training_steps_per_current_weight_update = $WEIGHTS_UPDATES' --argjson WEIGHTS_UPDATES $WEIGHTS_UPDATES <<<"$PARAMS")
+            echo $PARAMS > experiments/lunar_lander/parameters.json
 
-        launch_job/lunar_lander/launch_prodqn.sh -e $EXPERIMENT_NAME -fs 1 -ls 1 -b 5
+            launch_job/lunar_lander/launch_prodqn.sh -e $EXPERIMENT_NAME -fs 1 -ls 1 -b 5
+        done
     done
 done
--- launch_job/lunar_lander/launch_prodqn.sh
+++ launch_job/lunar_lander/launch_prodqn.sh
@@ -12,4 +12,4 @@ parse_arguments $@
 
 # ProDQN
 echo "launch train prodqn"
-submission_train_prodqn_1=$(sbatch -J L_$EXPERIMENT_NAME --array=$FIRST_SEED-$LAST_SEED --ntasks=4 --mem-per-cpu=2G --time=03:00:00 --gres=gpu:1 -p gpu --output=out/lunar_lander/$EXPERIMENT_NAME/$BELLMAN_ITERATIONS_SCOPE\_train_prodqn_%a.out launch_job/lunar_lander/train_prodqn.sh -e $EXPERIMENT_NAME -b $BELLMAN_ITERATIONS_SCOPE)
+submission_train_prodqn_1=$(sbatch -J L_$EXPERIMENT_NAME --array=$FIRST_SEED-$LAST_SEED --ntasks=4 --mem-per-cpu=2G --time=06:00:00 --gres=gpu:1 -p gpu --output=out/lunar_lander/$EXPERIMENT_NAME/$BELLMAN_ITERATIONS_SCOPE\_train_prodqn_%a.out launch_job/lunar_lander/train_prodqn.sh -e $EXPERIMENT_NAME -b $BELLMAN_ITERATIONS_SCOPE)
